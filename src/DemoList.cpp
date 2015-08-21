cc5d47f8610e4b4b0df4fb97edae9ce36c3d4b01
David Vargas <daviliyo96@gmail.com>
2015-08-21 22:28:53 +0200

    Create DemoList.cpp

---
+++ src/DemoList.cpp
@@ -0,0 +1,28 @@
+// DemoList.cpp
+
+#include "List.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// Creation of the list
+	// We need a pointer aiming to the first Node of it
+
+	cout << "Creating the list..." << endl;
+	Node * list = MakeNewList();
+	cout << endl;
+
+	// Shows the current list
+
+	cout << "Showing the list..." << endl;
+	ShowList (list);
+	cout << endl << endl;
+
+	// Cleans the memory
+	cout << "Cleaning the memory..." << endl;
+	CleanMemory (list);
+	cout << "Memory cleaned." << endl;
+	
+	return 0;
+}
