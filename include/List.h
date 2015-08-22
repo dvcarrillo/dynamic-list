e8e723ad404aa3a520dde9ab1617848a21d1a9c1
David Vargas <daviliyo96@gmail.com>
2015-08-22 02:55:02 +0200

    Update List.h
    Added some statistical functions.
    Code now looks more organized and easy to read.

--- include/List.h
+++ include/List.h
@@ -3,18 +3,29 @@
 #ifndef LIST 
 #define LIST
 
+// Define the type of data that the list is going to use
 typedef double DataType;
 
+// Struct dedicated to store the information of each node
 struct Node{
 	DataType info;
 	Node * next;
 };
 
-// Declaration of functions
+/*********************************************************/
+// FUNCTION PROTOTYPES
 
+// List management functions
 Node * MakeNewList (void);
 void ShowList (Node *);
 void CleanMemory (Node *);
+
+// List characteristics functions
 bool IsItVoid (Node *);
+int NumberOfNodes (Node *);
+
+// Statistical functions
+DataType Mean (Node *);
+DataType Variance (Node *);
 
 #endif
