bb09ec78616b096d242c3857680bfc04b19b7e23
David Vargas <daviliyo96@gmail.com>
2015-08-23 19:47:02 +0200

    Update DemoList.cpp
    Added some ordination functions

--- src/DemoList.cpp
+++ src/DemoList.cpp
@@ -16,16 +16,38 @@ int main(int argc, char const *argv[])
 	// Shows the current list
 
 	cout << "Showing the list..." << endl;
-	ShowList (list);
+
+	if (!IsItVoid(list))
+		ShowList (list);
+	else
+		cout << "This list is void.";
+
 	cout << endl << endl;
 
 	// Shows some operations on the list
 
+	cout << "STATISTICAL FUNCTIONS:" << endl;
 	cout << "* Number of nodes = " << NumberOfNodes(list);
 	cout << endl;
 	cout << "* Arithmetic mean = " << Mean(list);
 	cout << endl;
 	cout << "* Variance = " << Variance(list);
+	cout << endl << endl;
+
+	cout << "ORDINATION FUNCIONS:" << endl;
+	cout << "* List sorted: ";
+
+	if (IsItSorted(list))
+		cout << "yes (there's no need to sort)";
+	else
+	{
+		cout << "no" << endl << endl;
+		cout << "Sorting list..." << endl;
+		Sort(list);
+		cout << "Showing the sorted list..." << endl;
+		ShowList (list);
+	}
+
 	cout << endl << endl;
 
 	// Cleans the memory
