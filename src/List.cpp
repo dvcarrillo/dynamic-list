e3a98cd81c6143f424214464734fe1346382d1c3
David Vargas <daviliyo96@gmail.com>
2015-08-23 19:49:17 +0200

    Update List.cpp
    Added the "IsItSorted" and the "Sort" functions, which makes the program able to identify a sorted list and to sort a list.

--- src/List.cpp
+++ src/List.cpp
@@ -10,10 +10,10 @@ using namespace std;
 // Asks for new data until the user introduces a negative value, then it
 // finishes the list adding a null value on the "next" field of the last node.
 
-Node * MakeNewList (void)
+PNode MakeNewList (void)
 {
-	Node * first;	// Always points to the first node
-	Node * end;		// Points to the last node created
+	PNode first;	// Always points to the first node
+	PNode end;		// Points to the last node created
 
 	string msg = "Please enter a value (negative to finish): ";
 
@@ -64,9 +64,9 @@ Node * MakeNewList (void)
 // Show the values of the list of the argument (indicated with the pointer to
 // the first node), from the first to the last node.
 
-void ShowList (Node * first)
+void ShowList (PNode first)
 {
-	Node * aux = first;
+	PNode aux = first;
 
 	while (aux != 0)
 	{
@@ -79,7 +79,7 @@ void ShowList (Node * first)
 // CHECK IF THE LIST IS VOID
 // Receives a pointer to the first node of a list and checks if it is void
 
-bool IsItVoid (Node * first)
+bool IsItVoid (PNode first)
 {
 	return (first == 0);
 }
@@ -90,11 +90,11 @@ bool IsItVoid (Node * first)
 // before the program closes, or when needed.
 // Receives the pointer to the first node.
 
-void CleanMemory (Node * first)
+void CleanMemory (PNode first)
 {
 	if (!IsItVoid(first))		// Checks if the list is void
 	{
-		Node * aux = first;
+		PNode aux = first;
 
 		while (aux->next != 0)
 		{
@@ -112,13 +112,13 @@ void CleanMemory (Node * first)
 // Calculates the number of nodes in a list.
 // Receives as argument a pointer to the first node.
 
-int NumberOfNodes (Node * first)
+int NumberOfNodes (PNode first)
 {
 	int count = 0;		// Counts the number of nodes
 
 	if (!IsItVoid(first))		// Checks if the list is void
 	{
-		Node * aux = first;		// Auxiliar pointer that points to each node		
+		PNode aux = first;		// Auxiliar pointer that points to each node		
 
 		while (aux != 0)
 		{
@@ -135,13 +135,13 @@ int NumberOfNodes (Node * first)
 // Calculates the mean of all the values stored in the list.
 // Receives as argument a pointer to the first node.
 
-DataType Mean (Node * first)
+DataType Mean (PNode first)
 {
 	DataType mean = 0;
 
 	if (!IsItVoid(first))		// Checks if the list is void
 	{
-		Node * aux = first;
+		PNode aux = first;
 		DataType add = 0;		// Keeps the addition of the values
 
 		// Add the numbers to the variable until the list ends
@@ -163,13 +163,13 @@ DataType Mean (Node * first)
 // Calculates the variance of all the values stored in the list.
 // Receives as argument a pointer to the first node.
 
-DataType Variance (Node * first)
+DataType Variance (PNode first)
 {
 	DataType variance = 0;
 
 	if (!IsItVoid(first))		// Checks if the list is void
 	{
-		Node * aux = first;
+		PNode aux = first;
 		DataType add = 0;		// Keeps the addition of the values
 
 		// Add the numbers to the variable until the list ends
@@ -185,4 +185,72 @@ DataType Variance (Node * first)
 
 	return (variance);
 
+}
+
+/*****************************************************************************/
+// CHECK IF THE LIST IS SORTED
+// Returns "true" if the list is sorted. Returns "false" in other case.
+// A list is sorted when its numbers are stored from minor to major.
+// Receives as argument a pointer to the first node.
+
+bool IsItSorted (PNode first)
+{
+	bool sorted = true;
+
+	if (!IsItVoid(first))			// Checks if the list is void
+	{
+		PNode aux = first;
+
+		while ((aux->next) != 0)	// Compares all the values of the list
+		{
+			if ((aux->info) > ((aux->next)->info))
+				sorted = false;
+
+			aux = aux->next;
+		}
+	}
+
+	return(sorted);
+}
+
+/*****************************************************************************/
+// SORT A DISORDERED LIST
+// If a list is disordered, this function sorts it following the order "from
+// minor to major". It returns the pointer to the first node of it and 
+// receives as argument a pointer to the first node.
+
+void Sort (PNode &first)
+{
+	// Pointer which always points to the end of the "new sorted list" into
+	// the actual list
+	PNode aux1 = first;
+
+	if (!IsItSorted(first))			// Checks if the list is void
+	{
+		while (aux1->next != 0)
+		{			
+			DataType minor = aux1->info;
+			PNode minor_pos = aux1;
+
+			// Pointer which covers all the list and searchs the lowest value
+			PNode aux2 = aux1->next;
+
+			while (aux2 != 0)
+			{
+				if ((aux2->info) < minor)	// A new minor value is found
+				{
+					minor = aux2->info;
+					minor_pos = aux2;
+				}
+				aux2 = aux2->next;		// Goes on on the list
+			}
+
+			// Exchanges the minor for the value of the aux1 pointer
+			DataType tmp = aux1->info;
+			aux1->info = minor_pos->info;
+			minor_pos->info = tmp;
+
+			aux1 = aux1->next;
+		}
+	}
 }
